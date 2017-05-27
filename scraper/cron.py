cc9fda9fe6f1603df5039c6a97549d519d164f39
Terry Rodriguez <terry.j.rodriguez@gmail.com>
2017-05-27 12:35:00 -0400

    setup

--- scraper/cron.py
+++ scraper/cron.py
@@ -1,20 +1,23 @@
 def main():
     from crontab import CronTab
-
+    from os.path import expanduser
+    home = expanduser('~')
+    
+    cmd = 'python %s/apt_hunt/scraper/scraper.py' %s home
     cron = CronTab(user=True)
-    job = cron.new(command='python /home/ubuntu/apt_hunt/scraper/scraper.py')
+    job = cron.new(command=cmd)
     job.minute.on(0)
     job.hour.on(10)
 
-    job = cron.new(command='python /home/ubuntu/apt_hunt/scraper/scraper.py')
+    job = cron.new(command=cmd)
     job.minute.on(0)
     job.hour.on(13)
 
-    job = cron.new(command='python /home/ubuntu/apt_hunt/scraper/scraper.py')
+    job = cron.new(command=cmd)
     job.minute.on(0)
     job.hour.on(16)
 
-    job = cron.new(command='python /home/ubuntu/apt_hunt/scraper/scraper.py')
+    job = cron.new(command=cmd)
     job.minute.on(0)
     job.hour.on(19)
 
